d3179ff77102d0d461de2c4a46edafee8f144295
PengHoung <cheateng678@gmail.com>
2026-05-20 18:33:19 +0700

    feature_calendar: Add calendar feature to project
    # Conflicts:
    #	package-lock.json
    #	package.json

--- src/app/(main)/(calendar)/calendar/page.tsx
+++ src/app/(main)/(calendar)/calendar/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import CalendarEventComponent from "@/app/(main)/(calendar)/calendar/_components/CalendarEventComponent";
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <CalendarEventComponent />
+    </div>
   )
 }
