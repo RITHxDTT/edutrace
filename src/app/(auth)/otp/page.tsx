faebc6b125a9177602ca235fe9c6bf0c22987505
Uy-Chakriya <uychakriya002@gmail.com>
2026-05-25 14:02:43 +0700

    merge develop into feature/dashboardV1

--- src/app/(auth)/otp/page.tsx
+++ src/app/(auth)/otp/page.tsx
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
+
 import React from 'react'
-import VerificationForm from "./_components/Otpform";
+import VerificationForm from './_components/Otpform'
 
 export default function Page() {
       return (
@@ -8,16 +8,4 @@ export default function Page() {
                   <VerificationForm />
             </div>
       )
-=======
-
-import React from 'react'
-import VerificationForm from './_components/Otpform'
-
-export default function Page() {
-  return (
-    <div>
-      <VerificationForm />
-    </div>
-  )
->>>>>>> 8683e8f0978d4164688a479669b6c2aeda643645
 }
--- src/proxy.ts
+++ src/proxy.ts
@@ -23,5 +23,5 @@ export default async function proxy(req: NextAuthRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard", "//:path*", "/login", "/register"],
+  matcher: ["/login", "/register"],
 };
