eb644a36a2beaea0543861fc23f4038053202167
PengHoung <cheateng678@gmail.com>
2026-05-28 14:46:20 +0700

    feature/ai: update feature/ai part mock data

--- src/app/(main)/ai/page.tsx
+++ src/app/(main)/ai/page.tsx
@@ -1,9 +1,10 @@
-import AiChatWrapper from "@/components/AI/AiChatWrapper";
+import AiChatWrapper from "./_components/AI/AiChatWrapper";
+
 
 export default function Page(){
     return (
         <div className="relative w-full h-full bg-slate-50">
-            <AiChatWrapper />
+            <AiChatWrapper/>
         </div>
     );
 }
